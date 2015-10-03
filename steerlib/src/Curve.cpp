749485aa27d2901fa521e705766b57ba2fa4d33e
Ahmad <ahamidullah@gmail.com>
2015-10-03 15:23:18 -0400

    added Catmull Rom Curve (rough), fixed bug --  duplicate control nodes having the same time

--- steerlib/src/Curve.cpp
+++ steerlib/src/Curve.cpp
@@ -66,7 +66,6 @@ void Curve::drawCurve(Color curveColor, float curveThickness, int window)
 void Curve::sortControlPoints()
 {
 	/* simple bubblesort control points */
-	/* TODO(ahh42): optimize? */
 	int len = controlPoints.size();
 	bool swapped = false;
 	if(len < 2)
@@ -87,12 +86,23 @@ void Curve::sortControlPoints()
 			}
 		}
 	} while (swapped);
+	/* remove control points that have the same time */
+	for (int i = 0; i < controlPoints.size(); i++)
+	{
+		for (int j = i+1; j < controlPoints.size(); j++)
+		{
+			if (controlPoints[i].time == controlPoints[j].time)
+				controlPoints.erase(controlPoints.begin() + j);
+			else
+				break;
+		}
+	}
 #ifdef DEBUG
 	printf("Sorted Control Points: \n");
 	for (int i = 0; i < len; i++)
 	{
 		//printf("%f %f %f\n", controlPoints[i].position.x, controlPoints[i].position.y, controlPoints[i].position.z);
-		printf("\t%f\n", controlPoints[i].time);
+		//printf("\t%f\n", controlPoints[i].time);
 	}
 	printf("\n");
 #endif
@@ -141,34 +151,37 @@ bool Curve::checkRobust()
 // Find the current time interval (i.e. index of the next control point to follow according to current time)
 bool Curve::findTimeInterval(unsigned int& nextPoint, float time)
 {
-	int len = controlPoints.size();
 	if (time < 0.0f)
 		return false;
-	if(len < 1)
+	if(controlPoints.size() < 1)
+	{
 		return false;
+	}
 	if(time < controlPoints[0].time)
 	{
-		nextPoint = 0;
-		return true;
+		return false;
 	}
 
 	/* if the time is at or past the last control point return false */
-	if (time >= controlPoints[len-1].time)
+	if (time >= controlPoints[controlPoints.size()-1].time)
+	{
 		return false;
+	}
 
-	for (int i = 1; i < len; i++)
+	for (int i = 1; i <= controlPoints.size()-1; i++)
 	{
 		if (time < controlPoints[i].time && time >= controlPoints[i-1].time)
 		{
 			nextPoint = i;
 #ifdef DEBUG
 			printf("Current time: %f.\nChosen point index: %d, time: %f.\n", time, nextPoint, controlPoints[nextPoint].time);
-/*			for (int i = 0; i < len; i++)
+			for (int i = 0; i < controlPoints.size(); i++)
 			{
-				printf("\t%f\n", controlPoints[i].time);
+				//printf("\t%f %f %f %f\n", controlPoints[i].time, controlPoints[i].position.x, controlPoints[i].position.y, controlPoints[i].position.z);
 			}
-*/
+
 #endif
+
 			return true;
 		}
 	}
@@ -182,7 +195,6 @@ Point Curve::useHermiteCurve(const unsigned int nextPoint, const float time)
 	Point newPosition;
 	float normalTime, intervalTime;
 
-	// Calculate time interval, and normal time required for later curve calculations
 	intervalTime = controlPoints[nextPoint].time - controlPoints[nextPoint - 1].time;
 	/*
 	 * normalTime is the actual time normalized to be between 0 and 1, 0 being the time of the last control
@@ -195,41 +207,74 @@ Point Curve::useHermiteCurve(const unsigned int nextPoint, const float time)
 	if (normalTime < 0 || normalTime > 1)
 		return newPosition;
 
-	Point p0 = controlPoints[nextPoint - 1].position;
-	Point p1 = controlPoints[nextPoint].position;
-	Vector t0 = controlPoints[nextPoint - 1].tangent * intervalTime;
-	Vector t1 = controlPoints[nextPoint].tangent * intervalTime;
-	// Calculate position at t = time on Hermite curve
+	Point p1 = controlPoints[nextPoint - 1].position;
+	Point p2 = controlPoints[nextPoint].position;
+	/* tangents (need to be scaled by time interval between last control point and next control point) */
+	Vector s0 = controlPoints[nextPoint - 1].tangent * intervalTime;
+	Vector s1 = controlPoints[nextPoint].tangent * intervalTime;
 	/* based on the formula in the lecture slides (curves pg 64)... */
-	newPosition = (2*pow(normalTime, 3) - 3*pow(normalTime, 2) + 1)*p0 + (pow(normalTime, 3) - 2*pow(normalTime, 2) + normalTime)*t0 + (-2*pow(normalTime, 3) + 3*pow(normalTime, 2))*p1 + (pow(normalTime, 3) - pow(normalTime, 2))*t1;
+	newPosition = (2*pow(normalTime, 3) - 3*pow(normalTime, 2) + 1)*p1 + (pow(normalTime, 3) - 2*pow(normalTime, 2) + normalTime)*s0 + 
+		(-2*pow(normalTime, 3) + 3*pow(normalTime, 2))*p2 + (pow(normalTime, 3) - pow(normalTime, 2))*s1;
 
 #ifdef DEBUG
 	printf("Moving toward control point %f %f %f.\n", controlPoints[nextPoint].position.x, controlPoints[nextPoint].position.y, controlPoints[nextPoint].position.z);
-	printf("New position %f %f %f on curve.\n", newPosition.x, newPosition.y, newPosition.z);
+	//printf("New position %f %f %f on curve.\n", newPosition.x, newPosition.y, newPosition.z);
 #endif
-	// Return result
 	return newPosition;
 }
 
 // Implement Catmull-Rom curve
 Point Curve::useCatmullCurve(const unsigned int nextPoint, const float time)
 {
+	/* Catmull-Rom is just a hermite curve with a different tangent calculation (I think?) */
 	Point newPosition;
-
-	//================DELETE THIS PART AND THEN START CODING===================
-	static bool flag = false;
-	if (!flag)
-	{
-		std::cerr << "ERROR>>>>Member function useCatmullCurve is not implemented!" << std::endl;
-		flag = true;
-	}
-	//=========================================================================
-
+	float normalTime, intervalTime;
+	Point p1, p2;
+	Vector s1, s2;
 
 	// Calculate time interval, and normal time required for later curve calculations
+	intervalTime = controlPoints[nextPoint].time - controlPoints[nextPoint - 1].time;
+	/*
+	 * normalTime is the actual time normalized to be between 0 and 1, 0 being the time of the last control
+	 * point and 1 being the time of the next control point
+	*/
+	normalTime = (time - controlPoints[nextPoint - 1].time) / (intervalTime);
+#ifdef DEBUG
+	printf("Normal time: %f.\n", normalTime);
+#endif
+	if (normalTime < 0 || normalTime > 1)
+		return newPosition;
+	/*tangent calculation on the first and last control points are different */
+	if (nextPoint == 1)
+	{
+		p1 = controlPoints[nextPoint - 1].position;
+		p2 = controlPoints[nextPoint].position;
+		Point p3 = controlPoints[nextPoint + 1].position;
+		s1 = (2*((p2 - p1) / intervalTime) - ((p3 - p1) / (2*intervalTime))) * intervalTime;
+		s2 = ((p3 - p1) / (2*intervalTime)) * intervalTime;
+	}
+	else if (nextPoint == controlPoints.size()-1)
+	{
+		Point p0 = controlPoints[nextPoint - 2].position;
+		p1 = controlPoints[nextPoint - 1].position;
+		p2 = controlPoints[nextPoint].position;
+		//Point p3 = controlPoints[nextPoint + 1].position;
+		s1 = ((p2 - p0) / (2*intervalTime)) * intervalTime;
+		//s2 = (2*((p2 - p1) / intervalTime) - ((p3 - p1) / (2*intervalTime))) * intervalTime;
+		s2 = (2*((p2 - p0) / intervalTime) - ((p1 - p0) / (2*intervalTime))) * intervalTime;
+	}
+	else
+	{
+		Point p0 = controlPoints[nextPoint - 2].position;
+		p1 = controlPoints[nextPoint - 1].position;
+		p2 = controlPoints[nextPoint].position;
+		Point p3 = controlPoints[nextPoint + 1].position;
+		s1 = ((p2 - p0) / (2*intervalTime)) * intervalTime;
+		s2 = ((p3 - p1) / (2*intervalTime)) * intervalTime;
+	}
+	newPosition = (2*pow(normalTime, 3) - 3*pow(normalTime, 2) + 1)*p1 + (pow(normalTime, 3) - 2*pow(normalTime, 2) + normalTime)*s1 + 
+		(-2*pow(normalTime, 3) + 3*pow(normalTime, 2))*p2 + (pow(normalTime, 3) - pow(normalTime, 2))*s2;
 
-	// Calculate position at t = time on Catmull-Rom curve
 
-	// Return result
 	return newPosition;
 }
